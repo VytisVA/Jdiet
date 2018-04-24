4c675b91426d24c48a2cdc582976f9ee80890c22
Vytis Vadoklis <vytis.vadoklis@outlook.com>
2018-04-24 16:53:00 +0100

    complete change of structure, moving to js, jq, node.js

--- lib/jd_model.rb
+++
@@ -1,5 +0,0 @@
-class JDModel
-  def self.all
-      "Body weight management tool. Vegetarian and Vegan-friendly."
-  end
-end
--- spec/features/see_app_greeting_spec.rb
+++
@@ -1,6 +0,0 @@
-# feature 'Viewing app greeting' do
-#   scenario 'A user can see app greeting' do
-#     visit('/')
-#     expect(page).to have_content ("Body weight management tool. Vegetarian and Vegan-friendly.")
-#   end
-# end
