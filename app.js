c9d07e258a71bb535b31e27a1b22d2a039842bc0
Vytis Vadoklis <vytis.vadoklis@outlook.com>
2018-04-26 22:43:38 +0100

    working nodemon, installed ejs

--- app.js
+++ app.js
@@ -4,18 +4,41 @@ var path = require('path');
 
 var app = express();
 
-var logger = function(req, res, next){
-    console.log("Logging...");
-    next();
-}
+// var logger = function(req, res, next){
+//     console.log("Logging...");
+//     next();
+// }
 
-app.use(logger);
+// app.use(logger);
+
+
+// body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+//set static path
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+// var person = [
+//     {
+//         name: 'Jeff',
+//         age: 30
+//     },
+//     {
+//         name: 'Sara',
+//         age: 26
+//     }
+// ]
+
+// app.get('/', function(req, res){
+//     res.json(person)
+// });
 
 app.get('/', function(req, res){
-    res.send('Hfskfgjdshfgks!')
+    res.send("Hello")
 });
 
 app.listen(3000, function(){
     console.log("Server started on Port 3000...")
 })
-
